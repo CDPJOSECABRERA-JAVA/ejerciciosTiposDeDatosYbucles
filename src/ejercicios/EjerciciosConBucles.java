1d0e47eff324a4197ac813fc992009c6d3181b75
Cristian22g <criiss22g@gmail.com>
2024-10-15 11:13:39 +0200

    Hasta el 11

--- src/ejercicios/EjerciciosConBucles.java
+++ src/ejercicios/EjerciciosConBucles.java
@@ -36,23 +36,33 @@ public class EjerciciosConBucles {
     //Fibonacci es 0, el segundo es 1 y el resto se calcula sumando los dos anteriores, por lo que tendríamos que los términos
     //son 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144... El número n se debe introducir por teclado.
 
-    ejercicio6();
+    //ejercicio6();
 
     //7.- Escribe un programa que diga si un número introducido por teclado es o no primo. Un número primo es aquel que
     //sólo es divisible entre él mismo y la unidad.
+
+    //ejercicio7();
 
     //8.- Escribe un programa que obtenga los números enteros comprendidos entre dos números introducidos por teclado
     //y validados como distintos, el programa debe empezar por el menor de los enteros introducidos e ir incrementando
     //de 7 en 7.
 
+    //ejercicio8();
+
     //9.- Realiza un programa que vaya pidiendo números hasta que se introduzca un numero negativo y nos diga cuantos
     //números se han introducido, la media de los impares y el mayor de los pares. El número negativo sólo se utiliza para
     //indicar el final de la introducción de datos pero no se incluye en el cómputo.
+    
+    //ejercicio9();
 
     //10.- Realiza un programa que pida un número por teclado y que luego muestre ese número al revés.
 
+    //ejercicio10();
+
     //11.- Realiza un programa que pida primero un número y a continuación un dígito. El programa nos debe dar la posición
     //(o posiciones) contando de izquierda a derecha que ocupa ese dígito en el número introducido.
+
+    ejercicio11();
 
     //12.- Escribe un programa que calcule el factorial de un número entero leído por
     //teclado.
@@ -160,6 +170,134 @@ public class EjerciciosConBucles {
             puntero += fibonacci;
 
             num--;
+        }
+    }
+
+    public void ejercicio7(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número para saber si es primo o no:");
+        int num = sc.nextInt();
+        boolean esPrimo = true;
+
+        for(int i = 2; i <= num/2; i++) {
+            System.out.println(num + "%" + i + " = " + num%i);
+           if(num%i == 0){
+            esPrimo = false;
+            break;
+           }
+        }
+
+        if (esPrimo) System.out.println(num + " es un número primo.");
+        else System.out.println(num + " NO es un número primo");
+    }
+
+    public void ejercicio8(){
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int num2;
+        boolean sonIguales = false;
+
+        do {
+            System.out.println("Introduce el primer número:");
+            num1 = sc.nextInt();
+            System.out.println("Introduce el segundo número:");
+            num2 = sc.nextInt();
+
+            if (num1 == num2) {
+                System.out.println("Error. Los números no pueden ser iguales.");
+                sonIguales = true;
+            }else sonIguales = false;
+            
+            if (num1 < num2) {
+                int cambio = num1;
+                num1 = num2;
+                num2 = cambio;           
+            }
+        } while (sonIguales);
+
+        System.out.println("Incremento:");
+
+        for(int i = num2; i < num1; i+=7){
+            System.out.println(i);
+        }
+
+    }
+
+    public void ejercicio9(){
+        Scanner sc = new Scanner(System.in);
+        int sumaImpares = 0;
+        int mayorPar = 0;
+        int contadorNumeros = 0;
+
+        do{
+            System.out.println("Introduzca un número (si el número es negativo saldrá del programa).");
+            int num = sc.nextInt();
+
+            if (num < 0) {
+                System.out.println("El mayor par introducido es: " + mayorPar + ".");
+                System.out.println("La suma de los impares es de: " + sumaImpares + " y su media es de: " + sumaImpares/2);
+
+                System.out.println("Saliendo del programa...");
+                break;                
+            }
+
+            contadorNumeros++;
+
+            if (num % 2 == 0){
+                mayorPar = (num > mayorPar) ? num : mayorPar; 
+            }else{
+                sumaImpares += num;
+            }
+        
+        }while(true);
+    }
+
+    public void ejercicio10(){
+        Scanner sc =  new Scanner(System.in);
+
+        String numStringVolteado = "";
+        String numString = "";
+
+        System.out.println("Introduce el número que desea voltear");
+        int num = sc.nextInt();
+        numString += num; 
+        int numReducido= num;
+
+        do {
+            numStringVolteado += numReducido % 10;
+            numReducido /= 10;
+        } while (numReducido > 0);
+
+   
+        System.out.println(num + " es: " + numStringVolteado + " al revés.");
+  
+    }
+
+    public void ejercicio11(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un número:");
+        int num = sc.nextInt();
+        System.out.println("Ahora introduzca un dígito de ese número:");
+        int digito = sc.nextInt();
+
+        int contador = 0;
+        int digitoActual = 0;
+        int numRecortado = num;
+
+        while (true) {
+            digitoActual = numRecortado%10;
+            numRecortado = numRecortado/10;
+            contador++;
+
+            if (digito == digitoActual) {
+                System.out.println("La posición de ese dígito es la: " + contador);
+                break;
+            }
+            if (numRecortado < 0) {
+                System.out.println("Error. El dígito no ha sido encontrado en ese número.");
+                break;
+            }
         }
     }
 }
