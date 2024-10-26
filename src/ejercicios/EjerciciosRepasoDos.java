2a20bfd82163f4b0cb1780235ebeb2edefbfbb36
Criiss22g <criiss22g@gmail.com>
2024-10-26 19:14:47 +0200

    Todos hechos menos el 4

--- src/ejercicios/EjerciciosRepasoDos.java
+++ src/ejercicios/EjerciciosRepasoDos.java
@@ -7,21 +7,29 @@ public class EjerciciosRepasoDos {
         /* EJERCICIO 1
             Realiza un programa para jugar a “adivina el numero”.
         */
-        ejercicio1();
+
+        //ejercicio1();
+
         /* EJERCICIO 2
             Diseña un programa que resuelva ecuaciones de segundo grado.
         */
+
         //ejercicio2();
+
         /* EJERCICIO 3
             Diseña un programa que me diga los números elegantes que hay entre dos dados.
         */
-        //ejercicio3();
+
+        ejercicio3();
+
         /* EJERCICIO 4
             Diseña un programa que me diga los números primos sexis que hay entre dos dados.
         */
+
         //ejercicio4();
 
     }
+    
     public void ejercicio1(){
         Scanner sc = new Scanner(System.in);
         int num = 0;
@@ -58,14 +66,64 @@ public class EjerciciosRepasoDos {
     }
 
     public void ejercicio2(){
-        System.out.println("Ejercicio2");
+        //((-b +- Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2*a))
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Para solucionar una ecuación de segundo grado introduce el primer coeficiente (a):");
+        int a = sc.nextInt();
+        System.out.println("b:");
+        int b = sc.nextInt();
+        System.out.println("c:");
+        int c = sc.nextInt();
+
+        double raiz = (Math.sqrt((Math.pow(b,2) - (4*a*c))));
+
+        if (raiz < 0) {
+            System.out.println("Esta ecuación no tiene solución en números reales ya que su discriminante da un valor negativo.");
+            return;
+        }
+        double solucion1, solucion2;
+
+        solucion1 = (-b + raiz)/(2*a);
+        solucion2 = (-b - raiz)/(2*a);
+
+        String solucion1str = "" + solucion1, solucion2str = "" + solucion2;
+
+        if(solucion1str.equals("NaN") || solucion1str.equals("Infinity")){
+            System.out.println("La ecuación no tiene solucion en los números reales.");
+            return;
+        } 
+
+
+        System.out.printf("Solución 1: %.2f %n", solucion1);
+        System.out.printf("Solución 2: %.2f", solucion2);
     }
 
     public void ejercicio3(){
-        System.out.println("Ejercicio3");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el valor menor para obtener los números elegantes entre dos números.");
+        int numMenor = sc.nextInt();
+        System.out.println("Ahora el mayor:");
+        int numMayor = sc.nextInt();
+
+        if (numMayor < numMenor) {
+            int c = numMenor;
+            numMenor = numMayor;
+            numMayor = c;
+        }
+
+        for(int i = numMenor; i <= numMayor; i++){
+            if (comprobarNumeroElegante(i)) System.out.println(i);
+        }
     }
 
     public void ejercicio4(){
         System.out.println("Ejercicio4");
     }
+    
+    // -------------------------------------------------------------------------------------------------
+    boolean comprobarNumeroElegante(int i){
+    return true;
+    }
+
 }
+
